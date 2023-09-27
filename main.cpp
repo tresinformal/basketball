0515a3f7970891508142a1626072413b7d1d7248
oggyoggelito <oscar.ogg1999@gmail.com>
2023-09-27 18:45:49 +0200

    Minimal changes for main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
-#include <SFML/Graphics.hpp>
+//#include <SFML/Graphics.hpp>
 
 #include <cassert>
 #include <chrono>
 #include <iostream>
-
+#include <vector>
+#include <string>
 int main(int argc, char **argv)
 {
+    // Do something with args to not get a warning
+    const std::vector<std::string> args(argv, argv + argc);
+
     std::cout << "Hello Basketballers!\n";
 	return 0; // Game completed succesfully
+
 }
