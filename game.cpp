d0afa1ff19b7a642ef6aaa0fc7e5bf8142565cbb
martprenger <mart.prenger@gmail.com>
2023-10-11 19:00:40 +0200

    11 worked on issue 11

--- game.cpp
+++ game.cpp
@@ -1,18 +1,22 @@
 #include "game.h"
 
+
+
 game::game()
 {
 
+}
+
+int game::get_player_nb() {
+    return players.size();
 }
 
 void test_game()
 {
     // Here write tests for the game logic
     {
-#ifdef ISSUE_11
         // 11 - The game contains two players
         game g;
         assert(g.get_player_nb() == 2);
-#endif
     }
 }
--- game.h
+++ game.h
@@ -1,11 +1,16 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <vector>
+
+#include "player.h"
 
 class game
 {
 public:
     game();
+    std::vector<player> players(2);
+    int get_player_nb();
 };
 
 #endif // GAME_H
