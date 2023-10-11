9d57f7b5400ef8e0c9a9f0a5bafce475ce2f897a
Theo Pannetier <theo.pannetier@abdn.ac.uk>
2023-10-11 17:04:31 +0100

    preproc only encompasses #11

--- game.cpp
+++ game.cpp
@@ -7,14 +7,12 @@ game::game()
 
 void test_game()
 {
-
-#ifdef ISSUE_11
     // Here write tests for the game logic
     {
+#ifdef ISSUE_11
         // 11 - The game contains two players
         game g;
         assert(g.get_player_nb() == 2);
-    }
 #endif
-
+    }
 }
