86b820a6b2b44bcfa0c48afe833c9fa651fc7921
Theo Pannetier <theo@pannetier.nl>
2023-11-22 17:28:03 +0000

    fix issue #26

--- game.h
+++ game.h
@@ -9,7 +9,7 @@
 class game {
 
 public:
-    game(const int& screen_height, const int& screen_width);
+    game(const int& screen_height = 720, const int& screen_width = 1280);
     int get_n_players();
     ball get_ball() const;
     int get_screen_width() const;
