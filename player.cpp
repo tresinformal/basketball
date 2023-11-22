22dca776f3cc7acadb1034caf3763ac46f36cf54
Oscar Gullberg <oscar.ogg1999@gmail.com>
2023-11-22 17:28:55 +0100

    writing test

--- player.cpp
+++ player.cpp
@@ -16,6 +16,8 @@ void test_player() {
     const int player_score = player_g.get_score();
     //check player score
     assert(expected_player_score == player_score);
-
-
+    const double x { player_g.get_x() };
+    const double y { player_g.get_y() };
+    assert(x == 0.0);
+    assert(y == 0.0);
 }
